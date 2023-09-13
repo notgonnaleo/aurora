a32283d9d637b600b60eef45aa1c9a0ce07704d0
Leonardo <leo.bruni130@gmail.com>
2023-09-12 22:56:37 -0300

    product types

--- PCC/src/Backend/Backend.API/Controllers/Products/ProductTypeController.cs
+++ PCC/src/Backend/Backend.API/Controllers/Products/ProductTypeController.cs
@@ -1,20 +1,26 @@
+using Backend.API.Helpers.Controllers.Extensions;
 using Backend.Domain.Entities.Products;
+using Backend.Infrastructure.Services.Authorization;
 using Backend.Infrastructure.Services.Products;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class ProductTypeController : ControllerBase
+    [Route("ProductTypes")]
+    public class ProductTypeController : CustomController
     {
         private readonly ProductTypeService _productTypeService;
+        private readonly UserContextService _userContextService;
 
-        public ProductTypeController(ProductTypeService productTypeService)
+        public ProductTypeController(ProductTypeService productTypeService, UserContextService userContextService)
+        : base(userContextService)
         {
             _productTypeService = productTypeService;
+            _userContextService = userContextService;
         }
 
+        [ValidateUserContext]
         [HttpGet]
         [Route("List")]
         public async Task<ActionResult> Get()
@@ -30,6 +36,7 @@ namespace Backend.API.Controllers
             }
         }
 
+        [ValidateUserContext]
         [HttpGet]
         [Route("Find")]
         public async Task<ActionResult> GetById(Guid Id)
@@ -45,6 +52,7 @@ namespace Backend.API.Controllers
             }
         }
 
+        [ValidateUserContext]
         [HttpPost]
         [Route("Add")]
         public async Task<ActionResult> Add(ProductType productType)
@@ -61,6 +69,7 @@ namespace Backend.API.Controllers
             }
         }
 
+        [ValidateUserContext]
         [HttpPut]
         [Route("Update")]
         public async Task<ActionResult> Update(ProductType productType)
@@ -77,6 +86,7 @@ namespace Backend.API.Controllers
             }
         }
 
+        [ValidateUserContext]
         [HttpPut]
         [Route("Delete")]
         public async Task<ActionResult> Delete(Guid Id)
