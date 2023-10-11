d411374da3b5b44b024e1fd884849051eae2941f
Leonardo Bruni <leonardo.bruni@goenumerate.com>
2023-10-11 08:17:14 -0300

    app settings frontend working

--- src/Frontend/Frontend.Web/Util/Environments/EnvironmentHandler.cs
+++ src/Frontend/Frontend.Web/Util/Environments/EnvironmentHandler.cs
@@ -11,11 +11,9 @@ namespace Frontend.Web.Util.Environments
             _config = config;
         }
 
-        // ok just tested and its not getting the data from app settings
-        // TODO: Make these methods consume the appsettings in the frontend proj
         public string GetEnvironment()
         {
-            var environmentName = _config.GetSection("ApplicationSettings:Environment").Value;
+            var environmentName = _config.GetSection("ApplicationSettings")["Environment"].ToString();
 
             if (environmentName == null)
                 throw new ArgumentNullException(nameof(environmentName));
@@ -25,15 +23,18 @@ namespace Frontend.Web.Util.Environments
 
         public string GetEndpoint()
         {
+            // Suggestion: Maybe create an object for this information
+            // TODO: Create an ENUM for QA, Local and Prod flags.
             var environmentName = GetEnvironment();
             var environmentConfig = _config.GetSection("Environments")
-                .GetChildren()
-                .FirstOrDefault(x => x["Name"] == environmentName).Value;
+                                       .GetChildren()
+                                       .FirstOrDefault(x => x["Name"] == environmentName)
+                                       ?["Endpoint"];
 
             if (environmentConfig == null)
                 throw new ArgumentException($"Environment '{environmentName}' not found.");
 
-            return environmentConfig; // i think this shit will break but im too lazy to debug it rn
+            return environmentConfig;
         }
     }
 }
