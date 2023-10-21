5197c4fad838211a80639ac5255a2617859f57c2
Leonardo <lbruni10@gmail.com>
2023-10-21 14:34:50 -0300

    routes working properly

--- src/Frontend/Frontend.Web/Models/Route/RouteParameters.cs
+++ src/Frontend/Frontend.Web/Models/Route/RouteParameters.cs
@@ -6,11 +6,11 @@ namespace Frontend.Web.Models.Route
 {
     public class RouteParameters
     {
-        public string BuildParameterString(List<RouteParameters> routeParameters) 
+        public string BuildParameterString(List<RouteParameters> routeParameters)
         {
             string queryString = string.Empty;
 
-            if (routeParameters == null) 
+            if (routeParameters == null)
                 return queryString;
 
             for (int i = 0; i < routeParameters.Count(); i++)
@@ -18,7 +18,7 @@ namespace Frontend.Web.Models.Route
                 var parameter = routeParameters[i];
                 queryString += $"{parameter.ParameterName}={parameter.ParameterValue}";
 
-                if(i < routeParameters.Count - 1)
+                if (i < routeParameters.Count - 1)
                     queryString += "&";
             }
             return queryString;
@@ -32,6 +32,12 @@ namespace Frontend.Web.Models.Route
         // I'm still thinking about if I should implement a type discernation even though it will become an entire string in the end.
 
         public int Key { get; set; }
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+    }
+
+    public class RouteParameterRequest
+    {
         public string ParameterName { get; set; }
         public string ParameterValue { get; set; }
     }
--- src/Frontend/Frontend.Web/Services/Products/ProductService.cs
+++ src/Frontend/Frontend.Web/Services/Products/ProductService.cs
@@ -4,6 +4,8 @@ using Backend.Infrastructure.Enums.Modules;
 using Frontend.Web.Models.Route;
 using Frontend.Web.Repository.Client;
 
+using ProductsEnums = Backend.Infrastructure.Enums.Modules.Methods.Products;
+
 namespace Frontend.Web.Services.Products
 {
     public class ProductService
@@ -13,38 +15,66 @@ namespace Frontend.Web.Services.Products
         {
             _httpClientRepository = httpClientRepository;
         }
-        public void Send(params object[] parameters)
-        {
-            // Do something with the parameters
-            // For example, you can send them somewhere
 
-            foreach (var param in parameters)
-            {
-                Console.WriteLine($"Parameter: {param}");
-            }
-        }
-
-        /*
-        public RouteBuilder<Product> Send(string tenantId)
+        public RouteBuilder<Product> Send(RouteParameterRequest parameters)
         {
             return new RouteBuilder<Product>()
                 .BuildRoute(Endpoints.Products,
                 Methods.Default.GET,
                 new RouteParameters().BuildParameterString(new RouteParameters()
                 {
-                    Key = 1,
-                    ParameterName = "tenantId",
-                    ParameterValue = tenantId,
+                    Key = 1, // whatever
+                    ParameterName = parameters.ParameterName,
+                    ParameterValue = parameters.ParameterValue, // Right now i will treat everyone as a string, in the future i will make this type adaptable
                 }),
                 null);
         }
-        */
+
+        public RouteBuilder<Product> SendMultiple(List<RouteParameterRequest> parameters)
+        {
+            int key = 0;
+            var parameterRequestList = new List<RouteParameters>();
+
+            foreach (var parameter in parameters)
+            {
+                parameterRequestList.Add(new RouteParameters() 
+                { 
+                    Key = key,
+                    ParameterName = parameter.ParameterName,
+                    ParameterValue = parameter.ParameterValue
+                });
+                key++;
+            }
+            return new RouteBuilder<Product>()
+                .BuildRoute(Endpoints.Products, 
+                Methods.Default.FIND, 
+                new RouteParameters().BuildParameterString(parameterRequestList), 
+                null);
+        }
 
         public async Task<List<Product>> GetProducts(string tenantId)
         {
-            Send(tenantId); // maybe a dictionary? or a object again? idk this shit is confusing as fuck
-            //return await _httpClientRepository.Get<Product>(routeBuilder);
-            return new List<Product> { new Product() };
+            var request = Send(new RouteParameterRequest() { ParameterName = ProductsEnums.GET.GetProducts.tenantId, ParameterValue = tenantId });
+            return await _httpClientRepository.Get(request);
+        }
+
+        public async Task<Product> GetProduct(string tenantId, string productId)
+        {
+            var request = SendMultiple(new List<RouteParameterRequest>() 
+            {
+                new RouteParameterRequest()
+            {
+                ParameterName = ProductsEnums.GET.GetProduct.tenantId,
+                ParameterValue = tenantId,
+            },
+                new RouteParameterRequest()
+            {
+                ParameterName = ProductsEnums.GET.GetProduct.productId,
+                ParameterValue = productId,
+            }
+            });
+
+            return await _httpClientRepository.GetById(request);
         }
     }
 }
