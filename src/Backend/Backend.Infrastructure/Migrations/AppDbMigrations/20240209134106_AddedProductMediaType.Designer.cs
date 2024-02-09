840e22ff4be6094661d54b5d00bf6c0db1477c1b
SamuelFontes <samuguel@hotmail.com.br>
2024-02-09 07:52:26 -0600

    Added product media migration

--- src/Backend/Backend.Infrastructure/Migrations/AppDbMigrations/20240209134106_AddedProductMediaType.Designer.cs
+++ src/Backend/Backend.Infrastructure/Migrations/AppDbMigrations/20240209134106_AddedProductMediaType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Infrastructure.Migrations.AppDbMigrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240209134106_AddedProductMediaType")]
+    partial class AddedProductMediaType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
