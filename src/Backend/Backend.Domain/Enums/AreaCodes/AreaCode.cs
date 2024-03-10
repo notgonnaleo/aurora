9e32a3fc59c7cd3e6dedb92ade87bdbd13442ddc
Leonardo Bruni <leonardo.bruni@goenumerate.com>
2024-03-09 21:00:17 -0300

    phone

---
+++ src/Backend/Backend.Domain/Enums/AreaCodes/AreaCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Enums.AreaCodes
+{
+    public static class AreaCode
+    {
+        public static List<string> Codes = new List<string>()
+        {
+            "55",
+        };
+    }
+}
