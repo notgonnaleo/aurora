d7badc6193c8e9539ae12e2dc3b42ed431035d2f
Leonardo <lbruni10@gmail.com>
2024-04-04 01:11:03 -0300

    Enable Swagger (testing again)

--- src/Backend/Backend.API/Program.cs
+++ src/Backend/Backend.API/Program.cs
@@ -164,11 +164,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors(DevAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
