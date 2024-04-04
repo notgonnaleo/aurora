e4b7e581d8cc897c5b09366a939b93c9060ba9ea
Leonardo Bruni <leonardo.bruni@goenumerate.com>
2024-04-04 11:37:59 -0300

    more properties for customization wip

--- src/Backend/Backend.Domain/Entities/Reports/TemplateSettings.cs
+++ src/Backend/Backend.Domain/Entities/Reports/TemplateSettings.cs
@@ -8,9 +8,29 @@ namespace Backend.Domain.Entities.Reports
 {
     public class TemplateSettings
     {
-        // Title
+        // Report - General
+        public int ReportWidthSize { get; set; }
+        public int BorderSize { get; set; }
+        public string BorderColor { get; set; }
+
+        // Report - Title
+        public bool FontTitleBold { get; set; }
         public int FontTitleSize { get; set; }
         public string TitleAlign { get; set; }
+
+        // Report - Details
+        public string DetailsTextAlign { get; set; }
+
+        // Report - Table
+        public int ReportTableWidthSize { get; set; }
+
+        // Report - Table - Label
+        public int LabelTextSize { get; set; }
+        public string LabelTextColor { get; set; }
+
+        // Report - Table - Row
+        public int RowTextSize { get; set; }
+        public string RowTextColor { get; set; }
 
         public TemplateSettings() { }
 
