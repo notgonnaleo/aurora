5d264263f8c074113da2dcc943d15dd1039afcdc
Leonardo <lbruni10@gmail.com>
2024-04-14 15:42:57 -0300

    suffering to add radio button and creating settings page

---
+++ src/Frontend/Frontend.Web/Components/Settings/Theme/ThemeSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Backend.Domain.Enums.EnumExtensions;
+
+namespace Frontend.Web.Components.Settings.Theme
+{
+    public class ThemeSettings
+    {
+        public int ThemeId { get; set; }
+        public string ThemeName { get; set; }
+    }
+
+    public enum Themes
+    {
+        [Description("Dark")]
+        DarkMode = 0,
+        [Description("Light")]
+        LightMode = 1,
+    }
+}
