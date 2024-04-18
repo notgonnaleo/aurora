af9d1ef356cad39fc145cef5783e789d223c1c87
Leonardo Bruni <leonardo.bruni@goenumerate.com>
2024-04-18 15:31:33 -0300

    finished implementing in most layers and simplified it

--- src/Frontend/Frontend.Web/Models/Client/ApiResponse.cs
+++ src/Frontend/Frontend.Web/Models/Client/ApiResponse.cs
@@ -12,5 +12,10 @@ namespace Frontend.Web.Models.Client
         public T? Result { get; set; }
         public bool ResultBoolean { get; set; } // For now it's being used when the API response is boolean but the endpoint requires a type.
         public bool Success { get; set; }
+
+        public bool Successful()
+        {
+            return (Result is not null || ResultBoolean) && Success;
+        }
     }
 }
