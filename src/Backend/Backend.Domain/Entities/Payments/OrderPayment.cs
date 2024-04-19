0a659c5d0178d8524bd37cb237b013ce090d7309
Leonardo Bruni <leonardo.bruni@goenumerate.com>
2024-04-19 19:24:40 -0300

    whatever

---
+++ src/Backend/Backend.Domain/Entities/Payments/OrderPayment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Entities.Payments
+{
+    public class OrderPayment
+    {
+        public Guid OrderId { get; set; }
+        public Guid OrderPaymentId { get; set; }
+        public decimal OrderPaymentTotalAmount { get; set; }
+        public int OrderPaymentParcels { get; set; }
+        public PaymentType PaymentType { get; set; }
+    }
+
+    public class Payment
+    {
+        public Guid PaymentId { get; set; }
+        public Guid PayerId { get; set; }
+        public Guid SellerId { get; set; }
+        public DateTime PaymentEffectiveDate { get; set; }
+        public DateTime PaymentETA { get; set; }
+        public decimal TotalAmount { get; set; }
+        public PaymentType PaymentType { get; set; }
+    }
+
+    public class PaymentType
+    {
+        public int PaymentTypeId { get; set; }
+        public string PaymentTypeName { get; set; }
+    }
+}
