bf2a66206d9c59b90243f43b0a46a286df719b09
Leonardo <lbruni10@gmail.com>
2024-05-01 15:51:04 -0300

    place holders and better texts cleaning

--- src/Backend/Backend.Domain/Enums/StockMovements/MovementStatus.cs
+++ src/Backend/Backend.Domain/Enums/StockMovements/MovementStatus.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Backend.Domain.Enums.EnumExtensions;
 
 namespace Backend.Domain.Enums.StockMovements
 {
