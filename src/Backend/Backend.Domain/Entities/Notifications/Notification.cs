2f5198faf13004dd08051f24a5f5dba553962e7b
Leonardo <58894431+notgonnaleo@users.noreply.github.com>
2024-06-01 18:25:01 -0300

    Feature/notification bar (#77)
    * notificaton bar
    
    * de qlqr jeito msm pq tanto faz
    
    ---------
    
    Co-authored-by: Leonardo Bruni <leonardo.bruni@goenumerate.com>

---
+++ src/Backend/Backend.Domain/Entities/Notifications/Notification.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Domain.Entities.Agents.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Entities.Notifications
+{
+    public class Notification
+    {
+        /// <summary>
+        /// Entity id that will be used for redirect reference (Order, Product, Agent or Inventory)
+        /// </summary>
+        public Guid NotificationId { get; set; }
+        public int NotificationTypeId { get; set; }
+        public string? NotificationName { get; set; }
+        public string? NotificationDescription { get; set; }
+        public string? NotificationCustomerName { get; set; }
+        public string? NotificationEmployeeName { get; set; }
+        public string? NotificationItemCode { get; set; }
+        
+        /// <summary>
+        /// Product, Agent Type Id
+        /// </summary>
+        public int? NotificationItemTypeId { get; set; }
+        /// <summary>
+        /// Order and inventory status Id
+        /// </summary>
+        public int? NotificationItemStatusId { get; set; }
+
+        public DateTime NotificationTimestamp { get; set; }
+        /// <summary>
+        /// Created By OR Updated By Field
+        /// </summary>
+        public AgentThumbnailResponse NotificationActor { get; set; }
+    }
+}
